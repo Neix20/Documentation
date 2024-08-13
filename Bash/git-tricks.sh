a3d5d0f1a21a7b90d65002e3363d514edea21eb6
Neix20 <66442152+Neix20@users.noreply.github.com>
2024-08-13 15:30:26 +0800

    Create git-tricks.sh

---
+++ Bash/git-tricks.sh
@@ -0,0 +1,27 @@
+
+# How to create new Branch
+git checkout -b tmp
+
+# Delete Branch
+git branch -d tmp
+
+# How to completely copy local branch
+git switch -c tmp
+
+# How to Move Head of Branch Head to a Commit (e.g. tmp)
+git checkout tmp
+git reset --soft <commit-id>
+
+# How to Choose Specific Commits
+git rebase -i HEAD~<any-number>
+git rebase -i HEAD~6
+
+# Another Way of Doing This
+git checkout tmp
+git cherry-pick <commit-id>
+git cherry-pick <commit-id>
+
+# Another 2 way of doing this
+git checkout tmp
+git rebase -i <commid-id> master
+git rebase -i ecb82d40dfdff29e2515d602078242b19bdb3797 master
