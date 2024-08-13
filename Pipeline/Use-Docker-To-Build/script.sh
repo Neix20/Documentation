421ecf826596304fc4b35ed7d10f792936673602
Neix20 <txen2000@gmail.com>
2024-08-14 00:14:18 +0800

    feat: Added Use Docker to Build

---
+++ Pipeline/Use-Docker-To-Build/script.sh
@@ -0,0 +1,30 @@
+# Git Clone URL
+git clone https://github.com/Neix20/my-app.git
+
+# Docker Build Image
+docker build -t txe1/gay-img .
+
+############
+# Method 1
+############
+
+# Run Docker image
+docker run -p 3000:5173 --name simple-react-app -d txe1/gay-img
+
+# Test App
+curl localhost:3000
+
+# Copy Dist Folder
+docker create --name extract-dist txe1/gay-img
+docker cp extract-dist:/usr/src/app/dist .
+docker rm extract-dist
+
+############
+# Method 2
+############
+
+# This methods doesn't work :'(
+
+docker run -p 3000:5173 --name simple-react-app -v $(pwd)/output:/usr/src/app/dist -d txe1/gay-img
+docker run --rm -v ./output:/usr/src/app/dist -d txe1/gay-img
+
