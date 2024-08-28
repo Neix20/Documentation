c54a4ce247905641902a40bbb23a76737a8f840a
Neix20 <66442152+Neix20@users.noreply.github.com>
2024-08-29 02:03:00 +0800

    Create elk-stack.sh

---
+++ Kubernetes/elk-stack.sh
@@ -0,0 +1,28 @@
+
+#################################
+# ELK Setup on Kubernetes Cluster
+#################################
+
+# Setup Cluster Role Binding for ELK
+# Create the service account which has read access to service, endpoint and namespaces
+kubectl apply -f rbac.yml
+
+# Setup Elastic Search
+# Elastic Search is the CLI service used to manage logs
+kubectl apply -f elastic.yml
+kubectl apply -f elastic-service.yml
+
+# What is Log Stash Used for?
+# Logstash receives the logs and formate them in a way that Elasticsearch understand
+kubectl apply -f logstash-config.yml 
+kubectl apply -f logstash-deployment.yml
+
+# Once logstash is deployed successfully, we deploy the filebeat agent to ship the logs to Logstash. We will use the Daemonset kind of deployment which ensure that a pod will running on each node
+kubectl apply -f filebeat-daemon-set.yml
+
+# Deploy the Kibana now for log visualisation
+# This is the UI, similar to Grafana
+kubectl apply -f kibana.yml
+
+# Deploy Sample Deployment for Testing Kibana, Elastic Search
+kubectl apply -f web-deployment.yml
