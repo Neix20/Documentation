290a84121bfc5f12b66263968845c80a02a283c1
Neix20 <txen2000@gmail.com>
2026-01-04 17:21:31 +0800

    feat: Update

---
+++ nextJs/next.config.ts
@@ -0,0 +1,11 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  basePath: "/qabot",
+  assetPrefix: "/qabot/",
+  output: "standalone",
+  reactStrictMode: true,
+};
+
+export default nextConfig;
